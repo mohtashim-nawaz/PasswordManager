da20be967df16d5cc4db7ef63e808322798d9400
Stark <35381380+Stark-Tony@users.noreply.github.com>
2020-12-18 17:19:01 +0530

    Initial commit

---
+++ app/src/main/java/com/starklabs/passwordmanager/AddEditAcount.java
@@ -0,0 +1,14 @@
+package com.starklabs.passwordmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEditAcount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_acount);
+    }
+}
---
+++ app/src/main/java/com/starklabs/passwordmanager/ResetMasterPass.java
@@ -0,0 +1,14 @@
+package com.starklabs.passwordmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetMasterPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_master_pass);
+    }
+}
