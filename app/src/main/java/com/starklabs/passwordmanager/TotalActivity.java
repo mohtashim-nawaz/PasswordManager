1bd27b60414b4d03c52322335a8ae4803fa9ee40
Stark <35381380+Stark-Tony@users.noreply.github.com>
2020-12-19 05:18:55 +0530

    Implementation 3

--- app/src/main/java/com/starklabs/passwordmanager/TotalActivity.java
+++ app/src/main/java/com/starklabs/passwordmanager/TotalActivity.java
@@ -2,13 +2,29 @@ package com.starklabs.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TotalActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_total);
+
+        fab = findViewById(R.id.add_new_account);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TotalActivity.this, AddEditAcount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
